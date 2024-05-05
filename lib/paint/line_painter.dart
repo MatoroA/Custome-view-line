b67cf367a77fdc1d428fce303fe9a9e91df9cac3
Andani Matoro <andanimatoro@gmail.com>
2024-05-06 01:38:57 +0200

    custome loader

---
+++ lib/paint/line_painter.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class LinePainter extends CustomPainter {
+  final double progress;
+
+  LinePainter({this.progress = 0.5});
+
+  static final linePaint = Paint()
+    ..color = Colors.amber
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 6
+    ..strokeCap = StrokeCap.round;
+
+  static final linePaintTest = Paint()
+    ..color = Colors.green
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 6
+    ..strokeCap = StrokeCap.round;
+
+  static const _gap = 12.0;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height / 2;
+
+    final widthProgress = size.width * progress;
+    final point1 = max(0.0, widthProgress - _gap);
+    final point2 = min(widthProgress, size.width);
+
+    canvas.drawLine(Offset(0, height), Offset(point1, height), linePaint);
+    canvas.drawLine(Offset(point2 + _gap, height), Offset(size.width, height),
+        linePaintTest);
+  }
+
+  @override
+  bool shouldRepaint(LinePainter oldDelegate) {
+    return progress != oldDelegate.progress;
+  }
+}
